2f1f3b22fc34628128c090996a34fee92eeb4542
pharak123 <pharakaboli@gmail.com>
2023-04-19 15:16:17 +0530

    commiting Ex2 and Ex3

--- calculation.py
+++ calculation.py
@@ -7,6 +7,43 @@ def my_square(num1=4):
 def my_exponential(num1=2,num2=3):
     return num1**num2
 
+    return num1**num2
+def my_division(num1=2,num2=3):
+    return num1/num2
+def my_multiplication(num1=3,num2=6):
+    return num1*num2
+
+def accept_user():
+    return int(input("please enter the number :"))
+
+def my_calculator():
+    
+    option= int(input("Enter Your Option: "))
+    
+    if(option == 1):
+        #first_num = int(input("Please Enter first number: "))
+        #second_num = int(input("Please Enter second number: "))
+        # print("Addtion:",my_addtion(first_num,second_num))
+        print("Addtion:",my_addtion(accept_user(),accept_user()))
+
+    elif(option == 2):
+        # first_num = int(input("Please Enter first number: "))
+        
+        #print("Sqaure:",my_square(first_num))
+        print("Square:",my_square(accept_user()))
+    elif(option == 3):
+        # first_num = int(input("Please Enter first number: "))
+        # second_num = int(input("Please Enter second number: "))
+        # print("Exponential: ",my_exponential(first_num,second_num))
+        print("Exponential:",my_exponential(accept_user(),accept_user()))
+    elif(option == 4):
+        print("Division: ",my_division(accept_user(),accept_user()))
+    elif(option == 5):
+        print("Multiplication:",my_multiplication(accept_user(),accept_user()))
+    else:
+        print("Invalid Choice")
+    
+
 def my_input(num1,num2):
     #num1,num2=int(input("Enter the First Number: ")),int(input("Enter the Second Number: "))
     print("Addition : ",my_addtion(num1,num2))
@@ -53,3 +90,6 @@ def my_print(fare_messg):
     print("You are genius!!!")
    
     print(fare_messg)
+    
+    
+
